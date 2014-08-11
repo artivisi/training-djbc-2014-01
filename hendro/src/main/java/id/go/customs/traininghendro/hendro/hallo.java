431097329707fd252aa094a21b4e6e4e6ebb13f3
CEISA22 <CEISA22@CEISA22-HP>
2014-08-11 10:54:14 +0700

    script hallo word

---
+++ hendro/src/main/java/id/go/customs/traininghendro/hendro/hallo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package id.go.customs.traininghendro.hendro;
+
+/**
+ *
+ * @author CEISA22
+ */
+public class hallo {
+    public static void main(String[] args){
+   System.out.println("hallo");
+    
+}
+}
