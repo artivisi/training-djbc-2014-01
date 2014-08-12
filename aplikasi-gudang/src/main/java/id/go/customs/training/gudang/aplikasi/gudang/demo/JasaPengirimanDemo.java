58cda03af78d59096f154651c6a4d23506faca06
Dhea Junestya Pradipta <dheajunestya@gmail.com>
2014-08-12 10:26:53 +0700

    Commit Jasa Pengiriman

--- aplikasi-gudang/src/main/java/id/go/customs/training/gudang/aplikasi/gudang/demo/JasaPengirimanDemo.java
+++ aplikasi-gudang/src/main/java/id/go/customs/training/gudang/aplikasi/gudang/demo/JasaPengirimanDemo.java
@@ -14,19 +14,22 @@ import id.go.customs.training.gudang.aplikasi.gudang.domain.JasaPengiriman;
  */
 public class JasaPengirimanDemo {
     public static void main (String[] args){
+       
+        System.out.println("Jumlah Jasa Pengiriman (Awal): "+JasaPengiriman.getJumlahJasaPengiriman());
+       
         JasaPengiriman jp = new JasaPengiriman();
-   
-        jp.setId(1);
+        
         jp.setKodeJasaPengiriman("JP001");
         jp.setNamaJasaPengiriman("JNE");
         jp.setTipePengiriman("Express");
         jp.setKeterangan("1-2hari");
+        jp.tambahJasaPengiriman();
         
-        System.out.println("ID Jasa Pengiriman: "+jp.getId());
         System.out.println("Kode Jasa Pengiriman: "+jp.getKodeJasaPengiriman());
         System.out.println("Nama Jasa Pengiriman: "+jp.getNamaJasaPengiriman());
         System.out.println("Tipe Pengiriman: "+jp.getTipePengiriman());
         System.out.println("Keterangan: "+jp.getKeterangan());
+        System.out.println("Jumlah Jasa Pengiriman (Saat Ini): "+jp.getJumlahJasaPengiriman());
 
     }
 }
